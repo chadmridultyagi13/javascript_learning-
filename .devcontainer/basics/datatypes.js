4f53f1fc3581baa9b90c30e04ae1ea4d026594d4
chadmridultyagi13 <mridul_t@bt.iitr.ac.in>
2024-12-01 18:14:03 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict" ; // treat all js code newer version 
+
+// alert(3+3) -> yeh use karne ka different syntax hai kyonki abhi hum node ke andar kaam kar rahe hai na ki browser waale software mai
+// ; -> optional, one case -> console.log(3+3) ; console.log("mridul") ->yaha semicoloumn lagana chahiye varna error aa jata 
+// documentation -> mdn , original waala -> tc39.es 
+console.log("2+2") ;  
+let name = "mridul" ; 
+let age = 18 
+let bool = true 
+// number range -> 2 to power 53 
+// bigint 
+// string -> can be in "" or '' but prefer most of the times "" ; 
+// boolean -> true/false 
+// null -> standalone value , matlab koi value nhi hai , basically empty
+// undefined -> value define nhi kiya 
+// symbol -> to find uniqueness 
+// ------------------- primitive datatypes
+// object 
+console.log(typeof age) 
+// type of null => object 
+// type of undefined -> undefined 
+
