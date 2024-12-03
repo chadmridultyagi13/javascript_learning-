8686fa7c4f74edc3a91ed16edf03b45ab007e2d4
chadmridultyagi13 <mridul_t@bt.iitr.ac.in>
2024-12-03 13:33:09 +0000

    comparisons

--- .devcontainer/basics/comparisons.js
+++ .devcontainer/basics/comparisons.js
@@ -1,2 +1,12 @@
 // comparison will be done b/w two same datatypes , predictable results wiil not come 
+// console.log(2>=3) -> ans will come in true/false , >,<,<=,>=,!=  -> sab exist karta hai, same with string 
+// == has its own rules and doesn’t treat null as a numeric value and it treats null as NaN.Relational operators like >, <, >=, and <= coerce null into a numeric value (0) for the comparison., so answer of null>0 -> flase , null>=0 -> true , null==0 -> false 
+// == , aur baaki sab ka thoda alag tarika hai working ka 
+// undefined ka comparison with 0 ke case mai sabhi mai false aayega 
+// === -> strict check , this also checks value + datatype , "2"==2 -> can give true kyonki conversion hone lagta hai but "2"===2 -> will give false because datatypes are not same 
+
+
+
+
+
 
