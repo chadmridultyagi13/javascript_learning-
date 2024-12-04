a3865a8bccc232b71aa187183545f598f6b79dec
chadmridultyagi13 <mridul_t@bt.iitr.ac.in>
2024-12-04 07:10:02 +0000

    maths

---
+++ .devcontainer/basics/number_maths.js
@@ -0,0 +1,10 @@
+// an alternative way to declare number which guarantees that this is surely a number , const num = new Number(100) 
+// output of above -> [ Number : 100 ]
+//let num = 45.556 ; console.log((num.toFixed(2))) ; //  -> gives only two values after decimal -> 45.56, give in string datatype
+// toPrecision(3) -> gives precision upto 3 significant digits , basically approximate karke dega , and the datatype will be string
+// const num = 100000 -> console.log(num.toLocaleString())->add commas acc to american standard , to get the indian version give the parameter 'en-IN'
+// max and min value tu ek baar dekhlio 
+// ----------------maths------------------
+// browser ke console par ja , aur math ki bohot saari properties hongi jinko you can access by Math.(method/properties(like Pi)) 
+// console.log(Math.abs(-4))->4 , similarly other properties Math.round() , Math.ceil , floor, Math.min(2,3,4,5)-> 2 , similarly max . 
+// // Math.random()-> yeh waali cheez imp hai pls dekhlio ek baar 
