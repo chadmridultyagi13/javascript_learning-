bbe92104cd7bd92417733a1718b0eb588120e4dc
chadmridultyagi13 <mridul_t@bt.iitr.ac.in>
2024-12-06 12:03:05 +0000

    functions

--- .devcontainer/basics/functions.js
+++ .devcontainer/basics/functions.js
@@ -18,3 +18,25 @@ output -> please enter a username
 */
 // UNDEFINED,EMPTY STRING ETC KO IF ELSE KE ANDAR, BY DEFAUFLT FALSE MAANTE HAI 
 // EK DEFAULT VALUES KA BHI SCENE HAI, we can pass default values 
+// kaafi baar situation hoti hai ki kitne no of arguements pass karenge yeh bata nhi hota , isliye we have to adjust our parameters accordingly 
+/* so pata nhi hai kitne parameters hai we use rest operator ... (situation wise it can be called as spread operator as well as rest operator)
+ function calculateCartPrice(num1){
+  return num1 
+ }
+    ... -> use to pack the values in the bundle 
+    function fg(num1,num2 , ...num3){
+    return num3 
+    }
+    console.log(fg(12,13,14,15,16)) -> [14,15,16](array aayega)
+*/
+// how to use objects in function->
+/* const user = {
+username : "hitesh"
+prices : 199 
+}
+function handleobject(anyobject){
+ console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`)  -> mannle prices ki jagah price likh diya object ke andar then. yaha print undefined hoga prices waali jagah 
+ -> direct object bhi pass kar sakte hai as the arguement 
+
+}
+*/
