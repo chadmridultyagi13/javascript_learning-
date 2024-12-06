b952ef8d070a98cd21d8ca1a005f73c163328dc6
chadmridultyagi13 <mridul_t@bt.iitr.ac.in>
2024-12-06 11:18:40 +0000

    functions.js

---
+++ .devcontainer/basics/functions.js
@@ -0,0 +1,20 @@
+/*function saymyname(){
+console.log("mridul") ;
+}
+-> function declaration
+saymyname() -> calling/execution of the function
+*/
+// ek cheez hai -> 3 + null -> 3 , 3 + "a" - > 3a (yaha 3 ko string ki tarah treat kiya jayega) 
+// jab ham values hi pass nhi karte as a arguement , and function ke andar voh as  a parameter hai , then we get undefined 
+/* function loginusermessage(username){
+if(username===undefined){
+console.log("pls enter a username")
+return }
+return ` ${username} has just logged in ` 
+}
+console.log(loginUserMessage())
+output -> please enter a username 
+          undefined -> CAME DUE TO RETURN 
+*/
+// UNDEFINED,EMPTY STRING ETC KO IF ELSE KE ANDAR, BY DEFAUFLT FALSE MAANTE HAI 
+// EK DEFAULT VALUES KA BHI SCENE HAI, we can pass default values 
