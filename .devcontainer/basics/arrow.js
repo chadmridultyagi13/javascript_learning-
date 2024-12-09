b916d29e722b5a0104e634e9de8307708398859f
chadmridultyagi13 <mridul_t@bt.iitr.ac.in>
2024-12-09 11:21:40 +0000

    ARROW

--- .devcontainer/basics/arrow.js
+++ .devcontainer/basics/arrow.js
@@ -1,1 +1,17 @@
-// iske video dekhlio ,nhi banaye notes iske 
+// iske video dekhlio ,nhi banaye shuru ke notes iske 
+/*
+arrow function ->
+ const chai = (parameters as usual iss bracket mai daalne hai) =>{        
+    let username = "hitesh"
+    console.log(this.username)                     -> will print undefined when function will be called 
+    }
+    iski calling asusual chai() se hogi 
+-> the basic syntax of arrow function () => {} , aur iss pure ko kisi variable mai bhi store kar sakte hai 
+ 
+const addtwo = (num1,num2) => {
+    return num1+num2}
+console.log(addtwo(2,3)) 
+implicit return in arrow function -> 
+const addtwo = (num1,num2) => num1+num2 , agar paranthesis mai daal rahe hai then return keyword likhna mandatory hai
+OBJECT RETURN KARNA HAI TOH USKO PARANTHESIS MAI WRAP KARNA PADEGA 
+*/
